b717905df86ade0e9fc7849ed23f1e33f7921350
Katherine Angel <kt182_5@hotmail.com>
2020-09-20 22:05:55 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 /**
- * main - Alphabet
- *
- * Return: Always 0 (Success)
- */
+* main - Alphabet
+*
+* Return: Always 0 (Success)
+**/
 int main(void)
 {
-  char a = 'a';
-
-  while (a <= 'z')
-    {
-      putchar(a);
-      a++;
-    }
-  putchar('\n');
-  return (0);
+char a = 'a'; 
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
 }
