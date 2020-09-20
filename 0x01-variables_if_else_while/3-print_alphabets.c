b3919c9219b1e84026db719169fa2d1ef095ed6a
Katherine Angel <kt182_5@hotmail.com>
2020-09-20 21:57:42 +0000

    printalphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - Alphabet a-z and A-
- *
- * Return: Always 0 (Success)
- */
-
+* main - Alphabet a-z and A-
+*
+* Return: Always 0 (Success)
+**/
 int main(void)
 {
-  char a = 'a';
-
-  for (a = 'a'; a <= 'z'; a++)
-    {
-      putchar(a);
-    }
-  for (a = 'A'; a <= 'Z'; a++)
-    {
-      putchar(a);
-    }
-  putchar('\n');
-  return (0);
+char a = 'a';
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
 }
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
+
