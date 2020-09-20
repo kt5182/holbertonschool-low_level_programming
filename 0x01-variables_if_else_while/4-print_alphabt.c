64734730a5ab8365829fff97d011cd2109f43fc7
Katherine Angel <kt182_5@hotmail.com>
2020-09-20 00:23:32 +0000

    4alphabeth

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lower case exept q and e.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  char a;
+  for (a = 'a'; a <= 'z'; a++)
+    if (a != 'e' && a != 'q')
+      putchar(a);
+  putchar('\n');
+  return (0);
+}
