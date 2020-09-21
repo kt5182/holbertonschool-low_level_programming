961785ffba951d87b0f2fc63ab4164ac2c95fc39
Katherine Angel <kt182_5@hotmail.com>
2020-09-21 22:23:36 +0000

    print_number_hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Print numbers separeted with comas
+*
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+int h;
+for (h = 0; h <= 9; h++)
+{
+putchar(h + 48);
+}
+for (h = 'a'; h <= 'f'; h++)
+{
+putchar(h);
+}
+putchar('\n');
+return (0);
+}
