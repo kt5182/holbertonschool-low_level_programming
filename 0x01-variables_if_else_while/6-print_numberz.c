08ecd5d46db33df736915a58b2425a238611f660
Katherine Angel <kt182_5@hotmail.com>
2020-09-21 22:11:14 +0000

    single_digit_number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print numbers base 10
+*
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+int num = 0;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + 48);
+}
+putchar('\n');
+return (0);
+}
