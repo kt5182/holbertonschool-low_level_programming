0cdb11e97324d29ef98990f1113c11a7b59d7b49
Katherine Angel <kt182_5@hotmail.com>
2020-09-23 18:58:55 +0000

    fibonacci_sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main -this function display the fibo.
+*
+* Return: is 0
+**/
+int main(void)
+{
+int c;
+long a, b, d, e;
+a = 1;
+b = 2;
+e = 0;
+for (c = 0; c <= 31; c++)
+{
+if ((a < 4000000) && (a % 2 == 0))
+{
+e = e + a;
+}
+d = a + b;
+a = b;
+b = d;
+}
+printf("%ld", e);
+printf("\n");
+return (0);
+}
