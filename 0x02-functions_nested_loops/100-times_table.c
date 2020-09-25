fcb76f3660dcc6e0678cbc101b2d67a7873a388f
Katherine Angel <kt182_5@hotmail.com>
2020-09-25 22:31:00 +0000

    n_times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,41 +6,37 @@
 **/
 void print_times_table(int n)
 {
-int x = 0, y, z;
+int n1, n2, segnum, resul;  
 if (n > 15 || n < 0)
 return;
-while (x <= n)
+for (n1 = 0; n1 <= n; n1++)
 {
-for (y = 0; y <= n; y++)
+_putchar('0');
+for (n2 = 1; n2 <= n; n2++)
 {
-z = x y;
-if (z > 99)
-{
-_putchar(z / 100 + '0');
-_putchar((z / 10 % 10) + '0');
-_putchar(z % 10 + '0');
-}
-else if (z > 9)
-{
-_putchar(' ');
-_putchar(z / 10 + '0');
-_putchar(z % 10 + '0');
-}
-else if (y != 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(z + '0');
-}
-else
-_putchar(z + '0');
-if (y != n)
-{
+resul = n1 * n2;
 _putchar(',');
 _putchar(' ');
+if (resul <= 9)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(resul + '0');
+}
+else if (resul <= 99)
+{
+_putchar(' ');
+_putchar(resul / 10 + '0');
+_putchar(resul % 10 + '0');
+}
+else
+{
+segnum = ((resul / 10) % 10);
+_putchar(resul / 100 + '0');
+_putchar(segnum + '0');
+_putchar(resul % 10 + '0');
 }
 }
 _putchar('\n');
-x++;
 }
 }
