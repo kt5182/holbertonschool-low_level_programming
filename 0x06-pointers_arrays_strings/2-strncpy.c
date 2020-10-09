05b5bd37c0eaeca144b9d56b119fd94b63d0cd09
Katherine Angel <kt182_5@hotmail.com>
2020-10-09 16:51:49 +0000

    copies_string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,10 +8,16 @@
 **/
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-for (; n > i; i++)
-dest[i] = '\0';  
+int i = 0;
+while (i < n && *(src + i))
+{
+*(dest + i) = *(src + i);
+i++;
+}
+while (i < n)
+{
+*(dest + i) = '\0';
+i++;
+}
 return (dest);
 }
