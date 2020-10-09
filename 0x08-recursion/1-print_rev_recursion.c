5455ee272eb760e4c45e3e6b0bee56350f981af3
Katherine Angel <kt182_5@hotmail.com>
2020-10-09 21:35:57 +0000

    string_in_reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
 /**
-* _strlen_recursion - return length of a string
+* _print_rev_recursion - print a string in reverse
 * @s: string
-* Return: length of string
 **/
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-int c;
-c = 0;
 if (*s == '\0')
-return (0);
-c = 1 +  _strlen_recursion(s + 1);
-return (c);
+return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
