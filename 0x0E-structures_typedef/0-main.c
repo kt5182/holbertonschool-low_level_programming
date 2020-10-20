74b3e749b096c4e4aa9a7ebe432dffdb983d2abb
Katherine Angel <kt182_5@hotmail.com>
2020-10-20 23:42:04 +0000

    structure

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -12,7 +12,7 @@ int main(void)
 
   my_dog.name = "Django";
   my_dog.age = 3.5;
-  my_dog.owner = "Jay";
+  my_dog.owner = "Bob";
   printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
   return (0);
 }
