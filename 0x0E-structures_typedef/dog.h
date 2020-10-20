595d467e5663c025cd5ac532a992062c021bdc5a
Katherine Angel <kt182_5@hotmail.com>
2020-10-20 23:39:06 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - struct for dog info
+* @name: dog´s name
+* @age: dog´s age
+* @owner: dog´s owner
+**/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+#endif
