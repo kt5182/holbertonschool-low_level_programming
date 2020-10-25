837230b2eb31ef20bca1626b2cc2b5a156b4728a
Katherine Angel <kt182_5@hotmail.com>
2020-10-25 23:16:05 +0000

    memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
-* malloc_checked - allocate memory using malloc.
-* Description: If malloc fails, terminate process with status 98.
-* @b: unsigned int memory size to allocate
-* Return: void pointer to malloc'd memory space
+*malloc_checked - A function that allocates memory using malloc
+*@b: a variable that holds the input
+*Return: The address is returned.
 **/
 void *malloc_checked(unsigned int b)
 {
 void *p;
 p = malloc(b);
 if (p == NULL)
+{
 exit(98);
+}
 return (p);
 }
