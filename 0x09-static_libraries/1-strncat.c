8dfa68f591333d80d070c99a9c47b909234a13ba
Katherine Angel <kt182_5@hotmail.com>
2020-10-26 00:42:55 +0000

    library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _strncat - entry point
+* @dest:pointer
+* @src: pointer
+* Return: dest
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+(void)dest;
+(void)src;
+(void)n;
+return (dest);
+}
--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,5 @@
-#ifndef HOLBERTON
-#define HOLBERTON
-
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +20,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
 
-#endif /* HOLBERTON */
+
